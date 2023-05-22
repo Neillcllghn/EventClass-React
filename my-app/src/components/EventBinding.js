abb7fa8418c3256f69e5b5e89a1da9e97a2b604c
Neillcllghn <>
2023-05-22 18:26:37 +0000

    Ways to bind event handlers

---
+++ my-app/src/components/EventBinding.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+class EventBinding extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    // handleClick() {
+    //     this.setState({
+    //         introduction: "Goodbye!",
+    //         buttonText: "Enter",
+    //     });
+    //     console.log(this);
+    // }
+
+    handleClick = () => {
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: "Enter",
+        });
+        console.log(this);
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                {/* <button onClick={() => this.handleClick()}>{this.state.buttonText}</button> */}
+                {/* <button onClick={this.handleClick.bind(this)}>{this.state.buttonText}</button> */}
+                <button onClick={this.handleClick}>{this.state.buttonText}</button>
+            </div>
+        );
+    }
+}
+
+export default EventBinding;
